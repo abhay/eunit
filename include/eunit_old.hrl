483382b37dcdd1f5e051a842e1828e655b50e65b
rcarlsson <rcarlsson@5c723cce-c907-0410-9aa2-ab8348fff7a4>
2006-01-31 19:49:58 +0000

    New file containing old eunit 1.1 macros.
    git-svn-id: http://svn.process-one.net/contribs/trunk/eunit@22 5c723cce-c907-0410-9aa2-ab8348fff7a4

---
+++ include/eunit_old.hrl
@@ -0,0 +1,39 @@
+%% ``The contents of this file are subject to the Erlang Public License,
+%% Version 1.1, (the "License"); you may not use this file except in
+%% compliance with the License. You should have received a copy of the
+%% Erlang Public License along with this software. If not, it can be
+%% retrieved via the world wide web at http://www.erlang.org/.
+%%
+%% Software distributed under the License is distributed on an "AS IS"
+%% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+%% the License for the specific language governing rights and
+%% limitations under the License.
+%%
+%% The Initial Developer of the Original Code is Mickaël Rémond.''
+%%
+%%     $Id: eunit.hrl,v 1.1 2004/12/04 15:12:36 mremond Exp $
+%%
+
+%% Pre-2.0 EUnit macros
+
+-define(log(Format,Args), eunit_old:log(Format,Args,?FILE,?LINE)).
+-define(error(Format,Args), eunit_old:error(Format,Args,?FILE,?LINE)).
+
+-define(match(ExpectedRes,Expr),
+        fun() ->
+		ActualRes = (catch (Expr)),
+		case ActualRes of
+		    ExpectedRes -> ok;
+		    _ -> exit(test_case_failed)
+		end
+	end()).
+
+-define(match_inverse(NotExpectedRes,Expr),
+        fun() ->
+		ActualRes = (catch (Expr)),
+		case ActualRes of
+		    NotExpectedRes ->
+			exit(test_case_failed);
+		    _ -> ok
+		end
+	end()).
