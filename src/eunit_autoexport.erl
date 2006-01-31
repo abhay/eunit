c4f616fe8a08d0504d1023ba92e9ad8de2e01048
rcarlsson <rcarlsson@5c723cce-c907-0410-9aa2-ab8348fff7a4>
2006-01-31 19:12:19 +0000

    Added auto-exporting parse transform.
    git-svn-id: http://svn.process-one.net/contribs/trunk/eunit@19 5c723cce-c907-0410-9aa2-ab8348fff7a4

---
+++ src/eunit_autoexport.erl
@@ -0,0 +1,63 @@
+%% ``The contents of this file are subject to the Erlang Public License,
+%% Version 1.1, (the "License"); you may not use this file except in
+%% compliance with the License. You should have received a copy of the
+%% Erlang Public License along with this software. If not, it can be
+%% retrieved via the world wide web at http://www.erlang.org/.
+%%
+%% Software distributed under the License is distributed on an "AS IS"
+%% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+%% the License for the specific language governing rights and limitations
+%% under the License.
+%%
+%% The Initial Developer of the Original Code is Richard Carlsson.''
+%%
+%% File: eunit_autoexport.erl
+%%
+%% $Id:$ 
+%%
+%% @author Richard Carlsson <richardc@csd.uu.se>
+%% @copyright 2006 Richard Carlsson
+%% @private
+%% @see eunit
+%% @doc Parse transform for automatic exporting of test functions.
+
+-module(eunit_autoexport).
+
+-include("eunit_internal.hrl").
+
+-export([parse_transform/2]).
+
+
+parse_transform(Forms, Options) ->
+    TestSuffix = proplists:get_value(test_suffix, Options,
+				     ?DEFAULT_TEST_SUFFIX),
+    GeneratorSuffix = proplists:get_value(generator_suffix, Options,
+					  ?DEFAULT_GENERATOR_SUFFIX),
+    F = fun (Form, Set) ->
+		form(Form, Set, TestSuffix, GeneratorSuffix)
+	end,
+    Tests = sets:to_list(lists:foldl(F, sets:new(), Forms)),
+    export(Forms, Tests).
+
+form({function, _L, Name, 0, _Cs}, Tests, TestSuffix, GeneratorSuffix) ->
+    N = atom_to_list(Name),
+    case lists:suffix(TestSuffix, N) of
+	true ->
+	    sets:add_element({Name, 0}, Tests);
+	false ->
+	    case lists:suffix(GeneratorSuffix, N) of
+		true ->
+		    sets:add_element({Name, 0}, Tests);
+		false ->
+		    Tests
+	    end
+    end;
+form(_, Tests, _, _) ->
+    Tests.
+
+export([{attribute,L,module,_}=M | Fs], Names) ->
+    [M, {attribute,L,export,Names} | Fs];
+export([F | Fs], Names) ->
+    [F | export(Fs, Names)];
+export([], _Names) ->
+    [].  % just a sane fallback for safety
---
+++ src/eunit_internal.hrl
@@ -0,0 +1,43 @@
+%%%-------------------------------------------------------------------
+%% File: eunit_internal.hrl
+%%
+%% $Id:$ 
+%%
+%% @author Richard Carlsson <richard@csd.uu.se>
+%% @copyright 2006 Richard Carlsson
+%% @doc 
+
+%%-define(NOTEST, true).
+-define(NDEBUG, true).
+
+
+-define(DEFAULT_TEST_SUFFIX, "_test").
+-define(DEFAULT_GENERATOR_SUFFIX, "_test_").
+
+
+%% ---------------------------------------------------------------------
+%% Protocol records
+
+-ifndef(NDEBUG).
+-define(debugmsg(S),io:fwrite("\n* ~s: ~s\n", [?MODULE,S])).
+-define(debugmsg1(S,As),io:fwrite("\n* ~s: " ++ S ++ "\n", [?MODULE] ++ As)).
+-else.
+-define(debugmsg(S),ok).
+-define(debugmsg1(S,As),ok).
+-endif.
+
+-record(test, {f = undefined,
+	       desc = undefined,
+	       module = undefined,
+	       name = undefined,
+	       line = 0
+	      }).
+
+-record(context, {setup = undefined,
+		  cleanup = undefined,
+		  instantiate = undefined}).
+
+-record(group, {desc = undefined,	% optional description
+		spawn = undefined,	% run group in new process
+		order = undefined,	% run in order or in parallel
+		tests = undefined}).
