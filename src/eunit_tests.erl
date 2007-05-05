9626ea29baf210838974de29b7dded100ef5032b
rcarlsson <rcarlsson@5c723cce-c907-0410-9aa2-ab8348fff7a4>
2007-05-05 15:29:44 +0000

    a little cleanup and some added comments
    git-svn-id: http://svn.process-one.net/contribs/trunk/eunit@213 5c723cce-c907-0410-9aa2-ab8348fff7a4

--- src/eunit_tests.erl
+++ src/eunit_tests.erl
@@ -1,21 +1,35 @@
-%% ---------------------------------------------------------------------
-%% File: eunit_tests.erl
+%% This library is free software; you can redistribute it and/or modify
+%% it under the terms of the GNU Lesser General Public License as
+%% published by the Free Software Foundation; either version 2 of the
+%% License, or (at your option) any later version.
+%%
+%% This library is distributed in the hope that it will be useful, but
+%% WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+%% Lesser General Public License for more details.
+%%
+%% You should have received a copy of the GNU Lesser General Public
+%% License along with this library; if not, write to the Free Software
+%% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+%% USA
 %%
 %% $Id:$ 
 %%
 %% @author Richard Carlsson <richard@it.uu.se>
 %% @copyright 2007 Richard Carlsson
-%% @doc 
+%% @private
+%% @see eunit
+%% @doc External tests for eunit.erl
 
 -module(eunit_tests).
-
--export([]).
 
 -include("eunit.hrl").
 
 -ifdef(TEST).
 %% Cause all the other modules to be tested as well as this one.
 full_test_() ->
+    %%{application, eunit}.    % this currently causes a loop
+    %% We use the below until loop detection is implemented
     [eunit_autoexport,
      eunit_striptests,
      eunit_server,
